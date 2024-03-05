2c64507ddf79df682d1e2ebd76bc2a0c3a987812
Sergey020 <sergeytkachenko020@gmail.com>
2024-03-05 17:48:15 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,29 @@
 class Storage {
     #items;
-    constructor ({items}) {
+    constructor (items) {
         this.#items = items;
     }
+
+    //console.log(this.#items);
+
     getItems() {
+        console.log(this.#items);
         return this.#items;
     }
     addItem(newItem) {
-        return this.#items = newItem.push(this.#items);
+    this.#items.push(newItem);
     }
+removeItem(itemToRemove) {
+    this.#items.forEach((item, indexOfItem) => {
+       if (item === itemToRemove) {
+       this.#items.splice(indexOfItem,1);
+       } 
+    })
 }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
